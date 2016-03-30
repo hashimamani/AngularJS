ea945ffa8b9030085b6dc4aa0b83141a6a2487ef
Hashim <Hashim@HAmani.local>
2016-03-30 18:05:27 +0300

    commited

---
+++ angularapp.js
@@ -0,0 +1,28 @@
+/*
+ * Cytonn Technologies
+ *
+ * @author: Hashim Amani <hamani@cytonn.com>
+ *
+ * Project: AngularJS.
+ *
+ */
+
+
+
+// create angular app
+	var validationApp = angular.module('validationApp', []);
+
+	// create angular controller
+	validationApp.controller('mainController', function($scope) {
+
+		// function to submit the form after all validation has occurred			
+		$scope.submitForm = function() {
+
+			// check to make sure the form is completely valid
+			if ($scope.userForm.$valid) {
+				alert('Successful');
+			}
+
+		};
+
+	});
